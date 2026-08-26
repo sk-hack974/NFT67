c25cf5d25bbdee288995efce6b5c89e58ccf3866
sk-hack974 <sashakleshnya2013@gmail.com>
2026-08-26 21:56:17 +0300

    Update join.js

--- join.js
+++ join.js
@@ -1,11 +1,13 @@
 const joinBtn = document.getElementById("joinBtn");
 const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("closeBtn");
+const password = document.getElementById("password");
+const showPassword = document.getElementById("showPassword");
 // Відкрити вікно
 joinBtn.addEventListener("click", function () {
     modal.style.display = "flex";
 });
-// Закрити вікно
+// Закрити хрестиком
 closeBtn.addEventListener("click", function () {
     modal.style.display = "none";
 });
@@ -14,4 +16,14 @@ modal.addEventListener("click", function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
+});
+// Показати / сховати пароль
+showPassword.addEventListener("click", function () {
+if (password.type === "password") {
+    password.type = "text";
+    showPassword.textContent = "◉";
+} else {
+    password.type = "password";
+    showPassword.textContent = "○";
+}
 });
